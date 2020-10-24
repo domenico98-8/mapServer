71f9069bf31fdda00661fa136034e127e9584046
patre1 <patriziaconte31@gmail.com>
2020-10-24 16:27:56 +0200

    prova2

--- src/MainTest.java
+++ src/MainTest.java
@@ -10,7 +10,7 @@ import server.MultiServer;
 
 @SuppressWarnings("serial")
 public class MainTest implements Serializable{
-	//DOMY
+	//facciamo una prova
 	private static int port=8080;
 	/**
 	 * @param args
